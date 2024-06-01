c33578ff1cbc1ee1fd792e5b828389d4f91ece42
Amit08062001 <amitsingh19052001@gmail.com>
2024-06-01 12:22:48 +0530

    added some icon

--- src/Weather.jsx
+++ src/Weather.jsx
@@ -3,7 +3,6 @@ import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import { useState } from 'react';
 
-
 export default function Weather({updateInfo}){
     
     let [city , setCity] = useState("")
@@ -16,52 +15,66 @@ export default function Weather({updateInfo}){
       let result= await fetch(`${API_URL}?q=${city}&appid=${API_KEY}&units=metric`)
       let resResult = await result.json()
       console.log(resResult)
+      if (resResult.cod !== 200) {
+          throw new Error(resResult.message);
+      }
       let weatherResult = {
-           city :city,
+           city : city,
            temp : resResult.main.temp,
            weather: resResult.weather[0].description,
            minTemp : resResult.main.temp_min,
            maxTemp : resResult.main.temp_max,
-           FeelsLike :resResult.main.feels_like,
-           humidity :resResult.main.humidity
+           FeelsLike : resResult.main.feels_like,
+           humidity : resResult.main.humidity
       }
       console.log(weatherResult);
       return weatherResult
     }
     catch (error){
         throw error
-} 
+    } 
 }
-
 
      let handleInput= (event) => {
          setCity(event.target.value)
+         setError(false) 
      }
 
     let handleSubmit = async (event) =>{
         try{
             event.preventDefault()
             console.log(city)
-            setCity("")
+            setError(false) 
             let newInfo = await weatherInfo();
+            setCity("")
             updateInfo(newInfo)
         }
-       
-       catch(error){
-        setError(true)
-       } 
+        catch(error){
+            setError(true)
+        } 
     } 
+
     return(
         <form onSubmit={handleSubmit}>
             <h2>Weather Report</h2>
             <div>
-            <TextField id='city'  label="Enter City Name"  required value={city} onChange={handleInput}/> 
-            <br></br> <br></br>
-            <Button sx={{ backgroundColor :"blue"}} variant="contained " type='submit'>Search</Button>
-            {error && <p style={{color :"red"}}>No Such Place Exists</p>}
+                <TextField 
+                    id='city'  
+                    label="Enter City Name"  
+                    required 
+                    value={city} 
+                    onChange={handleInput}
+                /> 
+                <br></br><br></br>
+                <Button 
+                    sx={{ backgroundColor: "blue" }} 
+                    variant="contained" 
+                    type='submit'
+                >
+                    Search
+                </Button>
+                {error && <p style={{color: "red"}}>No Such Place Exists</p>}
             </div>
-            
-
         </form>
     )
 }
--- src/infoBox.jsx
+++ src/infoBox.jsx
@@ -3,22 +3,27 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import "./infoBox.css"
 import Typography from '@mui/material/Typography';
+import AcUnitIcon from '@mui/icons-material/AcUnit';
+import WbSunnyIcon from '@mui/icons-material/WbSunny';
+import ThunderstormIcon from '@mui/icons-material/Thunderstorm';
+
 
 export default function InfoBox({info}){
-   let  INIT_URL="https://images.unsplash.com/photo-1609881142760-298c2e76725b?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-   
-    return(
+   let  Rain_URL="https://images.unsplash.com/photo-1610741083757-1ae88e1a17f7?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+   let Hot_URL ="https://images.unsplash.com/photo-1447601932606-2b63e2e64331?q=80&w=1958&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+   let Cold_URL ="https://images.unsplash.com/photo-1564314968303-86c5df2b9a4c?q=80&w=1889&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" 
+   return(
         <div>
             
      <Card className="card" sx={{ maxWidth: 400 }}>
       <CardMedia
         sx={{ height: 200 }}
-        image={INIT_URL}
+        image={info.humidity > 80 ? Rain_URL : info.temp >15 ? Hot_URL : Cold_URL }
         title="green iguana"
       />
       <CardContent>
         <Typography gutterBottom variant="h5" component="div">
-          {info.city}
+          {info.city} &nbsp;{info.humidity > 80 ? <ThunderstormIcon/> : info.temp >15 ? <WbSunnyIcon/> : <AcUnitIcon/> }
         </Typography>
         <Typography variant="body2" color="text.secondary" component={"span"}>
           <p>Temprature : {info.temp}&deg;C</p>
